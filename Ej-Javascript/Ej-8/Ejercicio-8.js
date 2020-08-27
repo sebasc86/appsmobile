912576ef9a179bdb35e62784ab81f2436c4452b8
Sebastian Coscia <sebascoscia@gmail.com>
2020-08-27 19:24:13 -0300

    cambios funcion flecha

--- Ej-Javascript/Ej-8/Ejercicio-8.js
+++ Ej-Javascript/Ej-8/Ejercicio-8.js
@@ -1,23 +1,5 @@
 let array = [1, 2, 3, 4, 5];
 
-// Solucion 1
-
-/*
-var cont = 0;
-
-var cuantosCumplen = function (){
-    for (let i of array){
-        if (i % 2 === 0){
-            cont++;
-        }
-    }
-    return cont;
-}
-console.log(cuantosCumplen());
-*/
-
-// Solucion 2
-
 var even = (element) => element % 2 === 0;
 var cuantosCumplen = array.filter(even);
 
